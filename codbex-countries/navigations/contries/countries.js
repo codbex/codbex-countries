6219127055e667a0d9c6265013c54d3f0d2fb33b
TIVMOF <tomyivanov06@gmail.com>
2024-12-03 12:49:55 +0000

    feat: New Dashboard Structure

--- codbex-countries/navigations/contries/countries.js
+++ codbex-countries/navigations/contries/countries.js
@@ -2,9 +2,8 @@ const navigationData = {
     id: 'countries-navigation',
     label: "Countries",
     view: "countries",
-    group: "configurations",
-    orderNumber: 1000,
-    lazyLoad: true,
+    group: "reference-data",
+    orderNumber: 400,
     link: "/services/web/codbex-countries/gen/codbex-countries/ui/Countries/index.html?embedded"
 };
 
