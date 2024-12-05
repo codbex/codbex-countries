ded583598fba89618caad312a019fe84a7d0a175
TIVMOF <78173711+TIVMOF@users.noreply.github.com>
2024-12-05 12:32:57 +0200

    New Dashboard Structure (#17)

--- codbex-countries/navigations/countries/service.js
+++ codbex-countries/navigations/countries/service.js
@@ -1,10 +1,8 @@
 const navigationData = {
     id: 'countries-navigation',
     label: "Countries",
-    view: "countries",
-    group: "configurations",
-    orderNumber: 1000,
-    lazyLoad: true,
+    group: "metadata",
+    order: 400,
     link: "/services/web/codbex-countries/gen/codbex-countries/ui/Countries/index.html?embedded"
 };
 
