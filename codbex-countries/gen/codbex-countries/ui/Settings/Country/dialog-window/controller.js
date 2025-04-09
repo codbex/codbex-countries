c4570dacd50eea15b379f1816610c7695a190b87
StanZGenchev <stan.z.genchev@gmail.com>
2025-04-09 19:42:58 +0300

    Regenerated with new UI templates

---
+++ codbex-countries/gen/codbex-countries/ui/Settings/Country/dialog-window/controller.js
@@ -0,0 +1,86 @@
+angular.module('page', ['blimpKit', 'platformView', 'EntityService'])
+	.config(['EntityServiceProvider', (EntityServiceProvider) => {
+		EntityServiceProvider.baseUrl = '/services/ts/codbex-countries/gen/codbex-countries/api/Settings/CountryService.ts';
+	}])
+	.controller('PageController', ($scope, $http, ViewParameters, EntityService) => {
+		const Dialogs = new DialogHub();
+		$scope.entity = {};
+		$scope.forms = {
+			details: {},
+		};
+		$scope.formHeaders = {
+			select: 'Country Details',
+			create: 'Create Country',
+			update: 'Update Country'
+		};
+		$scope.action = 'select';
+
+		let params = ViewParameters.get();
+		if (Object.keys(params).length) {
+			$scope.action = params.action;
+			$scope.entity = params.entity;
+			$scope.selectedMainEntityKey = params.selectedMainEntityKey;
+			$scope.selectedMainEntityId = params.selectedMainEntityId;
+		}
+
+		$scope.create = () => {
+			let entity = $scope.entity;
+			entity[$scope.selectedMainEntityKey] = $scope.selectedMainEntityId;
+			EntityService.create(entity).then((response) => {
+				Dialogs.postMessage({ topic: 'codbex-countries.Settings.Country.entityCreated', data: response.data });
+				Dialogs.showAlert({
+					title: 'Country',
+					message: 'Country successfully created',
+					type: AlertTypes.Success
+				});
+				$scope.cancel();
+			}, (error) => {
+				const message = error.data ? error.data.message : '';
+				$scope.$evalAsync(() => {
+					$scope.errorMessage = `Unable to create Country: '${message}'`;
+				});
+				console.error('EntityService:', error);
+			});
+		};
+
+		$scope.update = () => {
+			let id = $scope.entity.Id;
+			let entity = $scope.entity;
+			entity[$scope.selectedMainEntityKey] = $scope.selectedMainEntityId;
+			EntityService.update(id, entity).then((response) => {
+				Dialogs.postMessage({ topic: 'codbex-countries.Settings.Country.entityUpdated', data: response.data });
+				Dialogs.showAlert({
+					title: 'Country',
+					message: 'Country successfully updated',
+					type: AlertTypes.Success
+				});
+				$scope.cancel();
+			}, (error) => {
+				const message = error.data ? error.data.message : '';
+				$scope.$evalAsync(() => {
+					$scope.errorMessage = `Unable to update Country: '${message}'`;
+				});
+				console.error('EntityService:', error);
+			});
+		};
+
+
+		$scope.alert = (message) => {
+			if (message) Dialogs.showAlert({
+				title: 'Description',
+				message: message,
+				type: AlertTypes.Information,
+				preformatted: true,
+			});
+		};
+
+		$scope.cancel = () => {
+			$scope.entity = {};
+			$scope.action = 'select';
+			Dialogs.closeWindow({ id: 'Country-details' });
+		};
+
+		$scope.clearErrorMessage = () => {
+			$scope.errorMessage = null;
+		};
+	});
