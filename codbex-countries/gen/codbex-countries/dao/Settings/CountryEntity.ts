01100a755572125d1aac72ef9473b055aca4998e
delchev <delchevn@gmail.com>
2025-10-08 19:11:34 +0300

    data store initial

---
+++ codbex-countries/gen/codbex-countries/dao/Settings/CountryEntity.ts
@@ -0,0 +1,7 @@
+export interface CountryEntity {
+    readonly Id: number;
+    Name: string;
+    Code2: string;
+    Code3: string;
+    Numeric: string;
+}
--- codbex-countries/gen/codbex-countries/dao/Settings/CountryRepository.ts
+++ codbex-countries/gen/codbex-countries/dao/Settings/CountryRepository.ts
@@ -1,26 +1,8 @@
 import { sql, query } from "sdk/db";
 import { producer } from "sdk/messaging";
 import { extensions } from "sdk/extensions";
-import { dao as daoApi } from "sdk/db";
-
-export interface CountryEntity {
-    readonly Id: number;
-    Name: string;
-    Code2: string;
-    Code3: string;
-    Numeric: string;
-}
-
-export interface CountryCreateEntity {
-    readonly Name: string;
-    readonly Code2: string;
-    readonly Code3: string;
-    readonly Numeric: string;
-}
-
-export interface CountryUpdateEntity extends CountryCreateEntity {
-    readonly Id: number;
-}
+import { store } from "sdk/db";
+import { CountryEntity } from "./CountryEntity";
 
 export interface CountryEntityOptions {
     $filter?: {
@@ -99,88 +81,49 @@ export interface CountryUpdateEntityEvent extends CountryEntityEvent {
 
 export class CountryRepository {
 
-    private static readonly DEFINITION = {
-        table: "CODBEX_COUNTRY",
-        properties: [
-            {
-                name: "Id",
-                column: "COUNTRY_ID",
-                type: "INTEGER",
-                id: true,
-                autoIncrement: true,
-            },
-            {
-                name: "Name",
-                column: "COUNTRY_NAME",
-                type: "VARCHAR",
-                required: true
-            },
-            {
-                name: "Code2",
-                column: "COUNTRY_CODE2",
-                type: "CHAR",
-                required: true
-            },
-            {
-                name: "Code3",
-                column: "COUNTRY_CODE3",
-                type: "CHAR",
-                required: true
-            },
-            {
-                name: "Numeric",
-                column: "COUNTRY_NUMERIC",
-                type: "CHAR",
-                required: true
-            }
-        ]
-    };
-
-    private readonly dao;
-
-    constructor(dataSource = "DefaultDB") {
-        this.dao = daoApi.create(CountryRepository.DEFINITION, undefined, dataSource);
-    }
+    private static readonly TABLE = "CODBEX_COUNTRY";
 
     public findAll(options: CountryEntityOptions = {}): CountryEntity[] {
-        let list = this.dao.list(options);
-        try {
-            let script = sql.getDialect().select().column("*").from('"' + CountryRepository.DEFINITION.table + '_LANG"').where('Language = ?').build();
-            const resultSet = query.execute(script, [options.$language]);
-            if (resultSet !== null && resultSet[0] !== null) {
-                let translatedProperties = Object.getOwnPropertyNames(resultSet[0]);
-                let maps = [];
-                for (let i = 0; i < translatedProperties.length - 2; i++) {
-                    maps[i] = {};
-                }
-                resultSet.forEach((r) => {
+        let list = store.list("CountryEntity", { 'conditions': [], 'limit': options.$limit || 20, 'offset': options.$offset || 0 });
+        if (options.$language) {
+            try {
+                let script = sql.getDialect().select().column("*").from('"' + CountryRepository.TABLE + '_LANG"').where('Language = ?').build();
+                const resultSet = query.execute(script, [options.$language]);
+                if (resultSet !== null && resultSet[0] !== null) {
+                    let translatedProperties = Object.getOwnPropertyNames(resultSet[0]);
+                    let maps: any[] = [];
                     for (let i = 0; i < translatedProperties.length - 2; i++) {
-                        maps[i][r[translatedProperties[0]]] = r[translatedProperties[i + 1]];
+                        maps[i] = {};
                     }
-                });
-                list.forEach((r) => {
-                    for (let i = 0; i < translatedProperties.length - 2; i++) {
-                        if (maps[i][r[translatedProperties[0]]]) {
-                            r[translatedProperties[i + 1]] = maps[i][r[translatedProperties[0]]];
+                    resultSet.forEach((r) => {
+                        for (let i = 0; i < translatedProperties.length - 2; i++) {
+                            maps[i][r[translatedProperties[0]]] = r[translatedProperties[i + 1]];
                         }
-                    }
+                    });
+                    list.forEach((r) => {
+                        for (let i = 0; i < translatedProperties.length - 2; i++) {
+                            if (maps[i][r[translatedProperties[0]]]) {
+                                r[translatedProperties[i + 1]] = maps[i][r[translatedProperties[0]]];
+                            }
+                        }
 
-                });
+                    });
+                }
+            } catch (Error) {
+                console.error("Entity is marked as language dependent, but no language table present: " + CountryRepository.TABLE);
             }
-        } catch (Error) {
-            console.error("Entity is marked as language dependent, but no language table present: " + CountryRepository.DEFINITION.table);
         }
         return list;
     }
 
     public findById(id: number, options: CountryEntityOptions = {}): CountryEntity | undefined {
-        const entity = this.dao.find(id);
-        if (entity) {
+        const entity = store.get("CountryEntity", id);
+        if (entity && options.$language) {
             try {
-                let script = sql.getDialect().select().column("*").from('"' + CountryRepository.DEFINITION.table + '_LANG"').where('Language = ?').where('Id = ?').build();
+                let script = sql.getDialect().select().column("*").from('"' + CountryRepository.TABLE + '_LANG"').where('Language = ?').where('Id = ?').build();
                 const resultSet = query.execute(script, [options.$language, id]);
                 let translatedProperties = Object.getOwnPropertyNames(resultSet[0]);
-                let maps = [];
+                let maps: any[] = [];
                 for (let i = 0; i < translatedProperties.length - 2; i++) {
                     maps[i] = {};
                 }
@@ -195,14 +138,14 @@ export class CountryRepository {
                     }
                 }
             } catch (Error) {
-                console.error("Entity is marked as language dependent, but no language table present: " + CountryRepository.DEFINITION.table);
+                console.error("Entity is marked as language dependent, but no language table present: " + CountryRepository.TABLE);
             }
         }
         return entity ?? undefined;
     }
 
-    public create(entity: CountryCreateEntity): number {
-        const id = this.dao.insert(entity);
+    public create(entity: CountryEntity): number {
+        const id = store.save('CountryEntity', entity);
         this.triggerEvent({
             operation: "create",
             table: "CODBEX_COUNTRY",
@@ -216,7 +159,7 @@ export class CountryRepository {
         return id;
     }
 
-    public update(entity: CountryUpdateEntity): void {
+    public update(entity: CountryEntity): void {
         const previousEntity = this.findById(entity.Id);
         this.dao.update(entity);
         this.triggerEvent({
@@ -232,24 +175,24 @@ export class CountryRepository {
         });
     }
 
-    public upsert(entity: CountryCreateEntity | CountryUpdateEntity): number {
-        const id = (entity as CountryUpdateEntity).Id;
+    public upsert(entity: CountryEntity): number {
+        const id = entity.Id;
         if (!id) {
-            return this.create(entity);
+            return store.save('CountryEntity', entity);
         }
 
-        const existingEntity = this.findById(id);
+        const existingEntity = store.get('CountryEntity', id);
         if (existingEntity) {
-            this.update(entity as CountryUpdateEntity);
+            this.update(entity);
             return id;
         } else {
-            return this.create(entity);
+            return store.save('CountryEntity', entity);
         }
     }
 
     public deleteById(id: number): void {
-        const entity = this.dao.find(id);
-        this.dao.remove(id);
+        const entity = store.get('CountryEntity', id);
+        store.remove('CountryEntity', id);
         this.triggerEvent({
             operation: "delete",
             table: "CODBEX_COUNTRY",
@@ -263,19 +206,7 @@ export class CountryRepository {
     }
 
     public count(options?: CountryEntityOptions): number {
-        return this.dao.count(options);
-    }
-
-    public customDataCount(): number {
-        const resultSet = query.execute('SELECT COUNT(*) AS COUNT FROM "CODBEX__COUNTRY"');
-        if (resultSet !== null && resultSet[0] !== null) {
-            if (resultSet[0].COUNT !== undefined && resultSet[0].COUNT !== null) {
-                return resultSet[0].COUNT;
-            } else if (resultSet[0].count !== undefined && resultSet[0].count !== null) {
-                return resultSet[0].count;
-            }
-        }
-        return 0;
+        return store.count('CountryEntity', options);
     }
 
     private async triggerEvent(data: CountryEntityEvent | CountryUpdateEntityEvent) {
@@ -285,7 +216,7 @@ export class CountryRepository {
                 triggerExtension.trigger(data);
             } catch (error) {
                 console.error(error);
-            }            
+            }
         });
         producer.topic("codbex-countries-Settings-Country").send(JSON.stringify(data));
     }
