1e2d76614c7899ba67f7a5aba4962a1e6fe5b79d
delchev <delchevn@gmail.com>
2025-10-15 13:16:45 +0300

    decorators added

--- codbex-countries/gen/codbex-countries/dao/Settings/CountryEntity.entity.ts
+++ codbex-countries/gen/codbex-countries/dao/Settings/CountryEntity.entity.ts
@@ -1,3 +1,4 @@
+import { Entity, Table, Id, Generated, Column } from "sdk/data";
 
 @Entity("CountryEntity")
 @Table("CODBEX_COUNTRY")
@@ -6,18 +7,18 @@ export class CountryEntity {
     @Id
     @Generated("sequence")
     @Column({ name: "COUNTRY_ID", type: "long" })
-    public Id: number;
+    public Id?: number;
 
     @Column({ name: "COUNTRY_NAME", type: "string" })
-    Name: string;
+    public Name?: string;
 
     @Column({ name: "COUNTRY_CODE2", type: "string" })
-    Code2: string;
+    public Code2?: string;
 
     @Column({ name: "COUNTRY_CODE3", type: "string" })
-    Code3: string;
+    public Code3?: string;
 
     @Column({ name: "COUNTRY_NUMERIC", type: "string" })
-    Numeric: string;
+    public Numeric?: string;
 
 }
