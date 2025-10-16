519ac2c49ea0083ac49644fb474f8e28f730c502
delchev <delchevn@gmail.com>
2025-10-16 15:57:59 +0300

    applied generic repository

--- codbex-countries/gen/codbex-countries/data/Settings/CountryRepository.ts
+++ codbex-countries/gen/codbex-countries/data/Settings/CountryRepository.ts
@@ -1,106 +1,13 @@
 import { producer } from "sdk/messaging";
 import { extensions } from "sdk/extensions";
-// import { store, Options } from "sdk/db";
-// import { translator } from "sdk/db";
-// import { EntityConstructor } from "sdk/db";
 import { CountryEntity } from "./CountryEntity";
-import { Repository, EntityEvent } from "./Repository";
-import { store, translator, Options, EntityConstructor } from "sdk/db";
-
-// export interface CountryEntityEvent {
-//     readonly operation: 'create' | 'update' | 'delete';
-//     readonly table: string;
-//     readonly entity: Partial<CountryEntity>;
-//     readonly key: {
-//         name: string;
-//         column: string;
-//         value: string | number;
-//     }
-//     readonly previousEntity?: CountryEntity;
-// }
+import { Repository, EntityEvent, EntityConstructor } from "sdk/db";
 
 export class CountryRepository extends Repository<CountryEntity> {
 
     constructor() {
         super((CountryEntity as EntityConstructor));
     }
-
-    // public findAll(options: Options = {}): CountryEntity[] {
-    //     let list = store.list((CountryEntity as EntityConstructor).$entity_name, options);
-    //     translator.translateList(list, options.language, (CountryEntity as EntityConstructor).$table_name);
-    //     return list;
-    // }
-
-    // public findById(id: number, options: Options = {}): CountryEntity | undefined {
-    //     const entity = store.get((CountryEntity as EntityConstructor).$entity_name, id);
-    //     translator.translateEntity(entity, id, options.language, (CountryEntity as EntityConstructor).$table_name);
-    //     return entity ?? undefined;
-    // }
-
-    // public create(entity: CountryEntity): string | number {
-    //     const id = store.save((CountryEntity as EntityConstructor).$entity_name, entity);
-    //     this.triggerEvent({
-    //         operation: "create",
-    //         table: (CountryEntity as EntityConstructor).$table_name,
-    //         entity: entity,
-    //         key: {
-    //             name: (CountryEntity as EntityConstructor).$id_name,
-    //             column: (CountryEntity as EntityConstructor).$id_column,
-    //             value: id
-    //         }
-    //     });
-    //     return id;
-    // }
-
-    // public update(entity: CountryEntity): void {
-    //     const previousEntity = this.findById(entity.Id as number);
-    //     store.update((CountryEntity as EntityConstructor).$entity_name, entity);
-    //     this.triggerEvent({
-    //         operation: "update",
-    //         table: (CountryEntity as EntityConstructor).$table_name,
-    //         entity: entity,
-    //         previousEntity: previousEntity,
-    //         key: {
-    //             name: (CountryEntity as EntityConstructor).$id_name,
-    //             column: (CountryEntity as EntityConstructor).$id_column,
-    //             value: entity.Id as number | string
-    //         }
-    //     });
-    // }
-
-    // public upsert(entity: CountryEntity): string | number {
-    //     const id = entity.Id;
-    //     if (!id) {
-    //         return store.save((CountryEntity as EntityConstructor).$entity_name, entity);
-    //     }
-
-    //     const existingEntity = store.get((CountryEntity as EntityConstructor).$entity_name, id);
-    //     if (existingEntity) {
-    //         this.update(entity);
-    //         return id;
-    //     } else {
-    //         return store.save((CountryEntity as EntityConstructor).$entity_name, entity);
-    //     }
-    // }
-
-    // public deleteById(id: number): void {
-    //     const entity = store.get((CountryEntity as EntityConstructor).$entity_name, id);
-    //     store.remove((CountryEntity as EntityConstructor).$entity_name, id);
-    //     this.triggerEvent({
-    //         operation: "delete",
-    //         table: (CountryEntity as EntityConstructor).$table_name,
-    //         entity: entity,
-    //         key: {
-    //             name: (CountryEntity as EntityConstructor).$id_name,
-    //             column: (CountryEntity as EntityConstructor).$id_column,
-    //             value: id
-    //         }
-    //     });
-    // }
-
-    // public count(options?: Options): number {
-    //     return store.count((CountryEntity as EntityConstructor).$entity_name, options);
-    // }
 
     protected async triggerEvent(data: EntityEvent<CountryEntity>) {
         const triggerExtensions = await extensions.loadExtensionModules("codbex-countries-Settings-Country", ["trigger"]);
