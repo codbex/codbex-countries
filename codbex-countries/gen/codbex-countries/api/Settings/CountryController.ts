c60ede08f96bbd5e018e24f85fe983b1fbf88a57
delchev <delchevn@gmail.com>
2025-10-17 23:36:57 +0300

    openapi fixes

--- codbex-countries/gen/codbex-countries/api/Settings/CountryController.ts
+++ codbex-countries/gen/codbex-countries/api/Settings/CountryController.ts
@@ -4,7 +4,7 @@ import { user } from "sdk/security"
 import { ForbiddenError, ValidationError } from "sdk/http/errors";
 import { HttpUtils } from "sdk/http/utils";
 import { Options } from "sdk/db";
-import { CountryEntity } from "../../data/Settings/CountryEntity";
+import { CountryEntity } from "/codbex-countries/gen/codbex-countries/data/Settings/CountryEntity";
 import { CountryRepository } from "../../data/Settings/CountryRepository";
 
 const validationModules = await Extensions.loadExtensionModules("codbex-countries-Settings-Country", ["validate"]);
@@ -79,7 +79,7 @@ class CountryController {
     }
 
     @Get("/:id")
-    public getById(_: any, ctx: any): CountryEntity {
+    public getById(id: number, ctx: any): CountryEntity[] {
         try {
             this.checkPermissions("read");
             const id = parseInt(ctx.pathParameters.id);
