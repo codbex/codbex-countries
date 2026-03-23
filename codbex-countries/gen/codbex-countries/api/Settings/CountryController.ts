a48d118ca7a586675f445237d0e6b4e61b102593
Yordan Pavlov <ThuF@users.noreply.github.com>
2026-03-23 15:37:26 +0200

    Migrate from project.json to package.json (#30)

--- codbex-countries/gen/codbex-countries/api/Settings/CountryController.ts
+++ codbex-countries/gen/codbex-countries/api/Settings/CountryController.ts
@@ -44,7 +44,7 @@ class CountryController {
             this.checkPermissions('write');
             this.validateEntity(entity);
             entity.Id = this.repository.create(entity) as any;
-            response.setHeader('Content-Location', '/services/ts/codbex-countries/gen/codbex-countries/api/Settings/CountryService.ts/' + entity.Id);
+            response.setHeader('Content-Location', '/services/ts/codbex-countries/gen/codbex-countries/api/Settings/CountryController.ts/' + entity.Id);
             response.setStatus(response.CREATED);
             return entity;
         } catch (error: any) {
